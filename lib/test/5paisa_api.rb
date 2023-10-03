6a2bb97013e0053fd7e1f04945e8601746aa1929
D P <pdebashis@outlook.com>
2023-10-03 10:08:48 +0530

    generic token

--- lib/test/5paisa_api.rb
+++ lib/test/5paisa_api.rb
@@ -1,6 +1,6 @@
 require 'websocket-client-simple'
 require 'json'
-socket_url = "wss://openfeed.5paisa.com/Feeds/api/chat?Value1=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjUzNTcwMDkzIiwicm9sZSI6IjE3NzIyIiwiU3RhdGUiOiIiLCJSZWRpcmVjdFNlcnZlciI6IkMiLCJuYmYiOjE2OTQ1NzcxNzksImV4cCI6MTY5NDYyOTc5OSwiaWF0IjoxNjk0NTc3MTc5fQ.HkioeyOplXClnEXiJeNefLbsm1I_Bsc1dVXN-VDltzY|53570093"
+socket_url = "wss://openfeed.5paisa.com/Feeds/api/chat?Value1=<token>|<client>"
 ws = WebSocket::Client::Simple.connect socket_url
 ws.on :open do puts "Connected" end
 ws.on :error do puts "Error" end
